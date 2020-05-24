8b8ee33630b04932319db66ac9e05c0d8217ce12
John Melody Me <Johnmelodyme@yandex.com>
2020-05-24 19:20:47 +0800

    Initial Commit

---
+++ Test Camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test Camera
+//
+//  Created by John Melody on 24/05/2020.
+//  Copyright © 2020 John Melody. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
